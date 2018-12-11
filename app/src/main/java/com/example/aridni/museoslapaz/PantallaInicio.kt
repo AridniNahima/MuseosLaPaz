64126f2ac54b309e1d5dd854a903ecaa21f5c3d9
indira <indirapoma_c@outlook.com>
2018-12-11 10:43:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aridni/museoslapaz/PantallaInicio.kt
@@ -0,0 +1,12 @@
+package com.example.aridni.museoslapaz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaInicio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_inicio)
+    }
+}
